015a2a1581bdca2df46a9bb95e348af3aa95a9e7
Kevin Kane <kkane@ncartmuseum.org>
2023-01-12 13:32:40 -0500

    fix to prevent timer from being started while running

--- examples/idle-timeout/idle-timer.js
+++ examples/idle-timeout/idle-timer.js
@@ -7,7 +7,8 @@
 // Define timeout functions for auto navigation back home on idle
 const idleTimeoutInMiliseconds = 5 * 1000; // 5s for development
 //const idleTimeoutInMiliseconds = 90 * 1000; // 90s for production
-let idleTimeoutId;
+
+let idleTimeoutId = 0; // initialize to 0
 
 function startIdleTimer() {
   // window.setTimeout returns an Id that can be used to start and stop a timer
@@ -31,22 +32,26 @@ function goActive() {
 
 function resetTimer() {
   window.clearTimeout(idleTimeoutId);
+  idleTimeoutId = 0; // setting to 0 allows setupIdleTimer to pass the check
   goActive();
 }
 
 function setupIdleTimer() {
-  console.log("Set up idle timer");
-  // Each of these events will reset the timer ("mousemove", "mousedown", etc)
-  window.addEventListener("mousemove", resetTimer, false); // fired at an element when a pointing device (usually a mouse) is moved while the cursor's hotspot is inside it
-  window.addEventListener("mousedown", resetTimer, false); // fired at an element when a pointing device button is pressed while the pointer is inside the element (differs from "click")
-  window.addEventListener("keydown", resetTimer, false); // fired when a key is pressed (for all keys, regardless of whether they produce a character value)
-  window.addEventListener("DOMMouseScroll", resetTimer, false); // deprecated for "wheel"
-  window.addEventListener("mousewheel", resetTimer, false); // deprecated for "wheel"
-  window.addEventListener("wheel", resetTimer, false); // fires when the user rotates a wheel button on a pointing device (typically a mouse)
-  window.addEventListener("touchmove", resetTimer, false); // fired when one or more touch points are moved along the touch surface
-  window.addEventListener("touchstart", resetTimer, false); // fired when one or more touch points are placed on the touch surface
-  window.addEventListener("pointermove", resetTimer, false); // fired when a pointer changes coordinates, and the pointer has not been canceled by a browser touch-action
-  startIdleTimer();
+  // check if running, idleTimeoutId will be > 0
+  if (!idleTimeoutId) {
+    console.log("Set up idle timer");
+    // Each of these events will reset the timer ("mousemove", "mousedown", etc)
+    window.addEventListener("mousemove", resetTimer, false); // fired at an element when a pointing device (usually a mouse) is moved while the cursor's hotspot is inside it
+    window.addEventListener("mousedown", resetTimer, false); // fired at an element when a pointing device button is pressed while the pointer is inside the element (differs from "click")
+    window.addEventListener("keydown", resetTimer, false); // fired when a key is pressed (for all keys, regardless of whether they produce a character value)
+    window.addEventListener("DOMMouseScroll", resetTimer, false); // deprecated for "wheel"
+    window.addEventListener("mousewheel", resetTimer, false); // deprecated for "wheel"
+    window.addEventListener("wheel", resetTimer, false); // fires when the user rotates a wheel button on a pointing device (typically a mouse)
+    window.addEventListener("touchmove", resetTimer, false); // fired when one or more touch points are moved along the touch surface
+    window.addEventListener("touchstart", resetTimer, false); // fired when one or more touch points are placed on the touch surface
+    window.addEventListener("pointermove", resetTimer, false); // fired when a pointer changes coordinates, and the pointer has not been canceled by a browser touch-action
+    startIdleTimer();
+  }
 }
 
 function removeIdleTimer() {
@@ -60,6 +65,7 @@ function removeIdleTimer() {
   window.removeEventListener("touchstart", resetTimer, false); // fired when one or more touch points are placed on the touch surface
   window.removeEventListener("pointermove", resetTimer, false); // fired when a pointer changes coordinates, and the pointer has not been canceled by a browser touch-action
   window.clearTimeout(idleTimeoutId);
+  idleTimeoutId = 0; // setting to 0 allows setupIdleTimer to pass the check
   console.log("removeIdleTimer called");
 }
 
